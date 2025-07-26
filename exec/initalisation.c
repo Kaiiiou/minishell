58c1798f9088745ef773648d834edda8525a2fb2
Alois <amarti@student.42lehavre.fr>
2025-07-27 01:08:04 +0200

    alloc pid & pipes

--- exec/initalisation.c
+++ exec/initalisation.c
@@ -28,5 +28,30 @@ void	exec_parsing_link(t_exec *exec, t_parsing *parsing)
 
 int	alloc_pids(t_exec *exec)
 {
+	if (!exec || exec->cmd_count <= 0)
+		return (0);
+	exec->pids = malloc (sizeof(pid_t) * (exec->cmd_count));
+	if(!exec->pids)
+		return (0);
+	return (1);
+}
 
+int	alloc_pipes(t_exec *exec)
+{
+	int	i;
+
+	i = 0;
+	if (!exec || exec->cmd_count <= 1)
+		return (0);
+	exec->pipes = malloc (sizeof(int *) * (exec->cmd_count - 1));
+	if(!exec->pipes)
+			return (0);
+	while(i < exec->cmd_count - 1)
+	{
+		exec->pipes[i] = malloc (sizeof(int) * 2);
+		if(!exec->pipes[i])
+			return (0);
+		i++;
+	}
+	return (1);
 }
